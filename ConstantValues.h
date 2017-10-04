8779c63646b3a8ff4b0187af0027e1843ccce5e0
FioKron <jmor@hotmail.co.uk>
2017-10-04 21:25:26 +0100

    Tutorial 03 Extra 02 (In progress)
    Still in progress, for working out issues with the vertices and their
    position/colour.

---
+++ ConstantValues.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <string>
+
+/** For the number of vertices present in g_vertices */
+const int VERTICES_COUNT = 8;
+
+/** For the Additional Exercises of Tutorial 02 */
+
+// Constant values:
+
+const int G_CLEAR_COLOUR_ARRAY_SIZE = 4;
+
+/** For Exercise 01 of Tutorial 3 */
+
+// Constant values:
+
+const float G_DEFAULT_CLEAR_COLOUR[G_CLEAR_COLOUR_ARRAY_SIZE] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+/** For Extra 01 of Tutorial 03 */
+
+// Constant values:
+
+/** For the respective Virtual-Key codes */
+
+// Numbers (not from the numpad):
+
+const int VK_T = 0x54;
+const int VK_Y = 0x59;
+const int VK_0 = 0x30;
+const int VK_1 = 0x31;
+const int VK_2 = 0x32;
+const int VK_3 = 0x33;
+const int VK_4 = 0x34;
+const int VK_5 = 0x35;
+const int VK_6 = 0x36;
+const int VK_7 = 0x37;
+const int VK_8 = 0x38;
+const int VK_9 = 0x39;
+
+// Leters:
+
+const int VK_AKEY = 0x41;
+const int VK_BKEY = 0x42;
+const int VK_CKEY = 0x43;
+const int VK_DKEY = 0x44;
+const int VK_EKEY = 0x45;
+const int VK_FKEY = 0x46;
+const int VK_GKEY = 0x47;
+const int VK_HKEY = 0x48;
+const int VK_IKEY = 0x49;
+const int VK_JKEY = 0x4A;
+const int VK_KKEY = 0x4B;
+const int VK_LKEY = 0x4C;
+const int VK_MKEY = 0x4D;
+const int VK_NKEY = 0x4E;
+const int VK_OKEY = 0x4F;
+const int VK_PKEY = 0x50;
+const int VK_QKEY = 0x51;
+const int VK_RKEY = 0x52;
+const int VK_SKEY = 0x53;
+const int VK_TKEY = 0x54;
+const int VK_UKEY = 0x55;
+const int VK_VKEY = 0x56;
+const int VK_WKEY = 0x57;
+const int VK_XKEY = 0x58;
+const int VK_YKEY = 0x59;
+const int VK_ZKEY = 0x5A;
+
+/** For modification of the Position and Colour of vertices */
+const float POSITION_MODIFICATION_SCALAR = 0.0010f;
+const float COLOUR_MODIFICATION_SCALAR = 0.010f;
+
+/**
+The bounds that position and colour values
+of vertices can tend to (either way)
+*/
+const float POSITION_LOWER_BOUND = -1.0f;
+const float POSITION_UPPER_BOUND = 1.0f;
+const float COLOUR_LOWER_BOUND = 0.0f;
+/**
+As on screen positions are valid between -1.0 and 1.0.
+Whilst colour values are valid between 0.0 and 1.0.
+*/
+const float COLOUR_UPPER_BOUND = POSITION_UPPER_BOUND;
+
+// Rename for each tutorial
+//char		g_TutorialName[100] = "James Moran Tutorial 02 Exercise 01\0";
+
+// (Attempting to use std::string instead):
+const std::string TUTORIAL_NAME = "James Moran Tutorial 03 Extra 01\0";
+
+// Give your app window your own name
+//char Name[100] = "James Moran\0";
+
+// (Changed to std::string once again, also made const.)
+const std::string NAME = "James Moran\0";
---
+++ Main.cpp
@@ -0,0 +1,540 @@
+#include <windows.h>
+#include <Windows.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <dxerr.h>
+
+#define _XM_NO_INTRINSICS_
+#define XM_NO_ALIGNMENT
+#include <xnamath.h>
+
+/** To use instead of an array of characters */
+#include <string>
+
+/** To assist in varying the screen-clear colour in Extra Exercise 01a */
+#include <random>
+
+//////////////////////////////////////////////////////////////////////////////////////
+//	Global Variables (seems as though g_ would equate to a global variable then
+//////////////////////////////////////////////////////////////////////////////////////
+HINSTANCE	g_hInst = NULL;
+HWND		g_hWnd = NULL;
+
+/** For Exercise 02 of Tutorial 01 */
+D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*           g_pD3DDevice = NULL;
+ID3D11DeviceContext*    g_pImmediateContext = NULL;
+IDXGISwapChain*         g_pSwapChain = NULL;
+
+/** For Exercise 01 of Tutorial 03 */
+ID3D11Buffer* g_pVertexBuffer;
+ID3D11VertexShader* g_pVertexShader;
+ID3D11PixelShader* g_pPixelShader;
+ID3D11InputLayout* g_pInputLayout;
+
+// 'Define vertex structure'
+struct POS_COL_VERTEX
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Col;
+};
+
+/** For exercise 01 of Tutorial 02 */
+ID3D11RenderTargetView* g_pBackBufferRTView = NULL;
+
+/** For the Additional Exercises of Tutorial 02 */
+
+// Constant values:
+
+const int G_CLEAR_COLOUR_ARRAY_SIZE = 4;
+
+/** For clearing the back buffer (leave the alpha value at 1.0f) */
+//float  g_clear_colour[G_CLEAR_COLOUR_ARRAY_SIZE] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+/** For the Advanced Exercise of Tutorial 02 */
+DXGI_SWAP_CHAIN_DESC DefaultSwapChainDescription;
+
+/** For Exercise 01 of Tutorial 3 */
+const float G_DEFAULT_CLEAR_COLOUR[G_CLEAR_COLOUR_ARRAY_SIZE] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+// Rename for each tutorial
+//char		g_TutorialName[100] = "James Moran Tutorial 02 Exercise 01\0";
+
+// (Attempting to use std::string instead):
+std::string TutorialName = "James Moran Tutorial 03 Exercise 01\0";
+
+//////////////////////////////////////////////////////////////////////////////////////
+//	Forward declarations
+//////////////////////////////////////////////////////////////////////////////////////
+HRESULT InitialiseWindow(HINSTANCE InstanceHandle, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+/** For Exercise 02 of Tutorial 01 */
+HRESULT InitialiseD3D();
+void ShutdownD3D();
+
+/** For Exercise 01 of Tutorial 02 */
+void RenderFrame(void); // (enquire about the use of void here, to identify no parameters)
+
+/** For exercise 01 of Tutorial 03 */
+HRESULT InitialiseGraphics(void); // void is void ^^
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Entry point to the program. Initializes everything and goes into a message processing 
+// loop. Idle time is used to render the scene.
+//////////////////////////////////////////////////////////////////////////////////////
+int WINAPI WinMain(HINSTANCE InstanceHandle, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (FAILED(InitialiseWindow(InstanceHandle, nCmdShow)))
+	{
+		DXTRACE_MSG("Failed to create Window");
+		return 0;
+	}
+
+	if (FAILED(InitialiseD3D()))
+	{
+		DXTRACE_MSG("Failed to create Device");
+		return 0;
+	}
+
+	if (FAILED(InitialiseGraphics()))
+	{
+		DXTRACE_MSG("Failed to initialise graphics");
+		return 0;
+	}
+
+	// Main message loop
+	MSG msg = { 0 };
+
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			RenderFrame();
+		}
+	}
+
+	// For closing Direct3D correctly:
+	ShutdownD3D();
+	return (int)msg.wParam;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Register class and create window
+//////////////////////////////////////////////////////////////////////////////////////
+HRESULT InitialiseWindow(HINSTANCE InstanceHandle, int nCmdShow)
+{
+	// Give your app window your own name
+	char Name[100] = "James Moran\0";
+
+	// Register class
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = InstanceHandle;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	//   wcex.hbrBackground = (HBRUSH )( COLOR_WINDOW + 1); // Needed for non-D3D apps
+	wcex.lpszClassName = Name;
+
+	if (!RegisterClassEx(&wcex)) return E_FAIL;
+
+	// Create window
+	g_hInst = InstanceHandle;
+	RECT rc = { 0, 0, 640, 480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	// As const char* can be parsed in as an LPCCHAR: 
+	g_hWnd = CreateWindow(Name, TutorialName.c_str(), WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left,
+		rc.bottom - rc.top, NULL, NULL, InstanceHandle, NULL);
+	if (!g_hWnd)
+		return E_FAIL;
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+	return S_OK;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Called every time the application receives a message
+//////////////////////////////////////////////////////////////////////////////////////
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	// For Extra Exercise 01b:
+	//float MouseXPosition = 0.0f;
+	//float MouseYPosition = 0.0f;
+	//float MouseWheelRotationDelta = 0.0f;
+
+	// For Advanced Exercise 01:
+
+	/** 
+		This result has been set to S_OK in execution so far 
+		(when calling ResizeBuffers).
+	*/
+	HRESULT ResizeResult = 0;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(g_hWnd);
+		return 0;
+
+	/**
+		// For altering the red...
+	case WM_MOUSEMOVE:
+		MouseXPosition = LOWORD(lParam);
+		MouseYPosition = HIWORD(lParam);
+
+		g_clear_colour[0] = (MouseXPosition / MouseYPosition) / 100.0f;
+		break;
+		// green...
+	case WM_LBUTTONDOWN:
+		g_clear_colour[1] += 0.010f;
+		break;
+
+	case WM_RBUTTONDOWN:
+		g_clear_colour[1] -= 0.010f;
+		break;
+
+		// as well as blue components of g_clear_colour.
+	case WM_MOUSEWHEEL:
+		
+		MouseWheelRotationDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+		
+		// Modify the blue component accordingly: 
+		g_clear_colour[2] += MouseWheelRotationDelta / 1000.0f;		
+		break;
+	*/
+	// For Tutorial 02 Advanced 01:
+	// CHECK TO SEE IF THIS IS THE OPTIMAL WAY TO GO ABOUT RESIZING THE BUFFERS!!OGea
+	case WM_SIZE:
+		if (g_pSwapChain)
+		{
+			// Clear the state of the device context first (so that ResizeBuffers() can succeed):
+			g_pImmediateContext->ClearState();
+
+			// Get the new bounds of the window:
+			RECT ClientBounds;
+			GetClientRect(g_hWnd, &ClientBounds);
+			UINT NewWidth = ClientBounds.right - ClientBounds.left;
+			UINT NewHeight = ClientBounds.bottom - ClientBounds.top;
+
+			/** 
+				Then it is possible to resize the buffers correctly
+				(Using DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH for the
+				last parameter to allowing switching between full-screen
+				and windowed mode. I'm also using DXGI_FORMAT_UNKNOWN, to
+				preserve the existing format. Finally, I have set the first 3
+				parameters as 0, to preserve the exitsting number of buffers,
+				as well as having DXGI use the width and height of the client
+				area of the target window, for then new width and height.)
+
+				SOURCE: https://msdn.microsoft.com/en-us/library/bb174577(v=vs.85).aspx 
+			*/
+			HRESULT ResizeResult = g_pSwapChain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN,
+				DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH);
+		}		
+		break;
+
+	default:
+		// A break-point was thrown on the return line, if hWnd is not valid:
+		// BREAKPOINT STILL THROWN AT TIMES GESU(GES[HJAD2!
+		if (hWnd)
+		{
+			// Add or replace the above check with that of checking this value: hWnd->unused;
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}		
+	}
+
+	/**
+	// Keep the colour components of g_clear_colour
+	// within the bounds (0.0f to 1.0f):	
+	for (int ClearColourIterator = 0; ClearColourIterator < G_CLEAR_COLOUR_ARRAY_SIZE - 1; ClearColourIterator++)
+	{
+		if (g_clear_colour[ClearColourIterator] >= 1.0f)
+		{
+			g_clear_colour[ClearColourIterator] = 1.0f;
+		}
+		else if (g_clear_colour[ClearColourIterator] <= 0.0f)
+		{
+			g_clear_colour[ClearColourIterator] = 0.0f;
+		}
+	}
+	*/
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Create D3D device and swap chain
+//////////////////////////////////////////////////////////////////////////////////////
+HRESULT InitialiseD3D()
+{
+	HRESULT hr = S_OK;
+
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE, // comment out this line if you need to test D3D 11.0 functionality on hardware that doesn't support it
+		D3D_DRIVER_TYPE_WARP, // comment this out also to use reference device
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC DefaultSwapChainDescription;
+	ZeroMemory(&DefaultSwapChainDescription, sizeof(DefaultSwapChainDescription));
+	DefaultSwapChainDescription.BufferCount = 1;
+	DefaultSwapChainDescription.BufferDesc.Width = width;
+	DefaultSwapChainDescription.BufferDesc.Height = height;
+	DefaultSwapChainDescription.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	DefaultSwapChainDescription.BufferDesc.RefreshRate.Numerator = 60;
+	DefaultSwapChainDescription.BufferDesc.RefreshRate.Denominator = 1;
+	DefaultSwapChainDescription.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	DefaultSwapChainDescription.OutputWindow = g_hWnd;
+	DefaultSwapChainDescription.SampleDesc.Count = 1;
+	DefaultSwapChainDescription.SampleDesc.Quality = 0;
+	DefaultSwapChainDescription.Windowed = true;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		g_driverType = driverTypes[driverTypeIndex];
+		hr = D3D11CreateDeviceAndSwapChain(NULL, g_driverType, NULL,
+			createDeviceFlags, featureLevels, numFeatureLevels,
+			D3D11_SDK_VERSION, &DefaultSwapChainDescription, &g_pSwapChain,
+			&g_pD3DDevice, &g_featureLevel, &g_pImmediateContext);
+		if (SUCCEEDED(hr))
+			break;
+	}
+
+	if (FAILED(hr))
+		return hr;
+
+	// Get pointer to back buffer texture
+	ID3D11Texture2D *pBackBufferTexture;
+	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D),
+		(LPVOID*)&pBackBufferTexture);
+
+	if (FAILED(hr)) return hr;
+
+	// Use the back buffer texture pointer to create the render target view
+	hr = g_pD3DDevice->CreateRenderTargetView(pBackBufferTexture, NULL,
+		&g_pBackBufferRTView);
+	pBackBufferTexture->Release();
+
+	if (FAILED(hr)) return hr;
+
+	// Set the render target view
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pBackBufferRTView, NULL);
+
+	// Set the viewport
+	D3D11_VIEWPORT viewport;
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = width;
+	viewport.Height = height;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+
+	g_pImmediateContext->RSSetViewports(1, &viewport);
+
+
+	return S_OK;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Clean up D3D objects
+//////////////////////////////////////////////////////////////////////////////////////
+void ShutdownD3D()
+{
+	if (g_pVertexBuffer) g_pVertexBuffer->Release();//'03-01'
+	if (g_pInputLayout) g_pInputLayout->Release();//'03-01'
+	if (g_pVertexShader) g_pVertexShader->Release();//'03-01'
+	if (g_pPixelShader) g_pPixelShader->Release();//'03-01'
+	if (g_pBackBufferRTView) g_pBackBufferRTView->Release();
+	if (g_pSwapChain) g_pSwapChain->Release();
+	if (g_pImmediateContext) g_pImmediateContext->Release();
+	if (g_pD3DDevice) g_pD3DDevice->Release();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Init graphics
+//////////////////////////////////////////////////////////////////////////////////////
+HRESULT InitialiseGraphics()//03-01
+{
+	HRESULT hr = S_OK;
+
+	// 'Define vertices of a triangle - screen coordinates -1.0f to +1.0'
+	// (Now the vertices for a square)
+	POS_COL_VERTEX vertices[] =
+	{
+		{XMFLOAT3(-0.9f, 0.9f, 0.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f)},
+		{XMFLOAT3(0.9f, 0.9f, 0.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f)},
+		{XMFLOAT3(0.9f, -0.9f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f)},
+		{XMFLOAT3(-0.9f, -0.9f, 0.0f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f)}
+	};
+
+	// 'Set up and create vertex buffer'
+	D3D11_BUFFER_DESC bufferDesc;
+	ZeroMemory(&bufferDesc, sizeof(bufferDesc));
+	bufferDesc.Usage = D3D11_USAGE_DYNAMIC; // 'Used by CPU and GPU'
+	bufferDesc.ByteWidth = sizeof(POS_COL_VERTEX) * 3; // 'Total size of buffer, 3 vertices'
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER; // 'Use as a vertex buffer'
+	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; // 'Allow CPU access'
+	hr = g_pD3DDevice->CreateBuffer(&bufferDesc, NULL, &g_pVertexBuffer); // 'Create the buffer'
+
+	if (FAILED(hr)) // return error code on failure
+	{
+		return hr;
+	}
+
+	// 'Copy the vertices into the buffer'
+	D3D11_MAPPED_SUBRESOURCE ms;
+
+	// 'Lock the buffer to allow writing'
+	g_pImmediateContext->Map(g_pVertexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);
+
+	// 'Copy the data'
+	memcpy(ms.pData, vertices, sizeof(vertices));
+
+	// 'Unlock the buffer'
+	g_pImmediateContext->Unmap(g_pVertexBuffer, NULL);
+
+	// 'Load and compile pixel and vertex shaders - use vs_5_0 to target DX11 hardware only'
+	ID3DBlob *VS, *PS, *error;
+	hr = D3DX11CompileFromFile("shaders.hlsl", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, &error, 0);
+	
+	if (error != 0) // 'check for shader compilation error'
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr)) // 'don't fail is error is just a warning'
+		{
+			return hr;
+		};
+	}
+
+	hr = D3DX11CompileFromFile("shaders.hlsl", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, &error, 0);
+	
+	if (error != 0) // 'check for shader compilation error'
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr)) // 'don't fail is error is just a warning'
+		{
+			return hr;
+		};
+	}
+
+	// 'Create shader objects'
+	hr = g_pD3DDevice->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &g_pVertexShader);
+	
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	hr = g_pD3DDevice->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &g_pPixelShader);
+	
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	// 'Set the shader objects as active'
+	g_pImmediateContext->VSSetShader(g_pVertexShader, 0, 0);
+	g_pImmediateContext->PSSetShader(g_pPixelShader, 0, 0);
+
+	// 'Create and set the input layout object'
+	D3D11_INPUT_ELEMENT_DESC iedesc[] =
+	{
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+
+	hr = g_pD3DDevice->CreateInputLayout(iedesc, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &g_pInputLayout);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	g_pImmediateContext->IASetInputLayout(g_pInputLayout);
+
+	return S_OK;
+}
+
+// Render frame
+void RenderFrame(void)
+{
+	// Vary the clear colour value:
+	//std::default_random_engine DefaultRandomGenerator(std::random_device{}());
+	// (By default, such a distribtion has a range from 0.0f to 1.0f, but I wanted
+	// to make this clear):
+	//std::uniform_real_distribution<float> RandomDistribution(0.0f, 1.0f); 
+
+	//for (int ColourComponentIterator = 0; ColourComponentIterator < G_CLEAR_COLOUR_ARRAY_SIZE - 1; ColourComponentIterator++)
+	//{
+		//g_clear_colour[ColourComponentIterator] = RandomDistribution(DefaultRandomGenerator);
+	//}
+
+	g_pImmediateContext->ClearRenderTargetView(g_pBackBufferRTView, G_DEFAULT_CLEAR_COLOUR);
+
+	// 'Set vertex buffer //03-01'
+	UINT stride = sizeof(POS_COL_VERTEX);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	// 'Select which primitive type to use //03-01'
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// 'Draw the vertex buffer to the back buffer //03-01'
+	g_pImmediateContext->Draw(3, 0);
+
+	// RENDER HERE
+
+	// Display what has just been rendered
+	g_pSwapChain->Present(0, 0);
+}
+
